a6000b7c32852bb40a22b1765e754f1bd57a3cd7
Vermond <paser2@gmail.com>
2021-05-18 15:00:12 +0900

    - Add missing file

---
+++ SpamProtector/View/Main/Search/SearchTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchTableCell.swift
+//  SpamProtector
+//
+//  Created by Jinsu Gu on 2021/05/18.
+//
+
+import Foundation
+
+import UIKit
+
+class SearchTableCell: UITableViewCell, ClassName {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
